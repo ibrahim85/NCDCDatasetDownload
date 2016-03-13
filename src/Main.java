a1a0203eb79988d10e1a99e6343dfff2bb125569
yvesbastos <raphael.bastos@me.com>
2016-03-13 14:21:12 -0300

    Instructions

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,11 @@ public class Main {
 		String[] dateRange = {"2010-01-01", "2010-01-05"};
 		String token = "insert your NCDC web services token here";
 		 
+		// Instructions:
+		// 1) Delete files inside data/datasets 
+		// 2) Delete data/successfulPulls.txt file
+		
+		
 		DatasetDownloader datasetsDownloader = new DatasetDownloader(token, datasets, dateRange, true);
 	}
 	
